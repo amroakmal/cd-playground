7f83efd6a23d0256e42bb0f579d0931c4dbd4068
AmrAkmal <amrakmal97@gmail.com>
2021-05-03 01:32:34 +0200

    fixes

--- src/index.js
+++ src/index.js
@@ -3,21 +3,21 @@ const app = express();
 const axios = require('axios')
 
 const NAME = process.env.NAME || 'default service name';
-const PORT = process.env.PORT || '3000';
+const PORT = process.env.PORT;
 
 app.get('/health', function(req, res) {
     res.send({
         NAME,
         PORT,
-        message: "Hiiii"
+        message: "/health end point"
     })
 })
 
 app.get('/deep-health', async function(req, res) {
     try {
         //from docs and here: https://zetcode.com/javascript/axios/
-        const {data: res1} = await axios.get(`${process.env.POSTS_URL}/health`);
-        const {data: res2} = await axios.get(`${process.env.VOTES_URL}/health`);
+        const {data: res1} = await axios.get(`${process.env.POSTS_URL}:${PORT}/health`);
+        const {data: res2} = await axios.get(`${process.env.VOTES_URL}:${PORT}/health`);
         res.send({
             res1,
             res2
